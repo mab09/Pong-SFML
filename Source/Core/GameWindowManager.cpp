dbc1fdd3425eb307a1f8d8f0708fc6dcd7ad4753
KageRyu <m.alibeg@gmail.com>
2024-12-28 15:31:51 +0530

    Added fullscreen capability

--- Source/Core/GameWindowManager.cpp
+++ Source/Core/GameWindowManager.cpp
@@ -11,9 +11,14 @@ namespace Core
 
     void GameWindowManager::createGameWindow() {
         // Create the window with specified dimensions and title
-        game_window->create(
+        /*game_window->create(
             VideoMode(game_window_width, game_window_height),
             game_title
+        );*/
+        game_window->create(
+            sf::VideoMode::getDesktopMode(),    // Get screen resolution
+            game_title,                        // Window title
+            sf::Style::Fullscreen               // Fullscreen mode
         );
     }
 
@@ -24,7 +29,13 @@ namespace Core
 
     void GameWindowManager::render() {
         // This will handle all the drawing operations
-        //It'll be used in future lessons
+            // Clear window with orange color (R:200, G:50, B:50, A:255)
+        game_window->clear(sf::Color(50, 50, 50, 255));
+
+        //draw shapes, sprites, etc 
+
+        // Display the changes
+        game_window->display();
     }
 
     RenderWindow* GameWindowManager::getGameWindow() {
